b9f77ded42271cd5fcedff42294f33ce694232ec
kb-dev-sketch <kb61406@gmail.com>
2026-04-14 18:10:40 +0530

    discuss util code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import connectDB from "./db/index.js";
 
+import app from "./app.js";
 
-connectDB();
-
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+  })
+  
+})
+.catch((error)=>{
+  console.log("mongo db connection failed !!",error);
+})
 
 
 /*
