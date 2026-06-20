dae14a61ea0d0ffdd0ee0848b265ccb43a55f71a
kb-dev-sketch <kb61406@gmail.com>
2026-06-20 17:04:28 +0530

    refactor: organize user routes and improve code readability

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,27 +1,49 @@
-import {Router} from "express";
-import {loginUser, logoutUser, registerUser} from "../controllers/user.controller.js"
+import { Router } from "express";
+import {
+    changeCurrentPassword,
+    getCurrentUser,
+    loginUser,
+    logoutUser,
+    registerUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateCoverImage,
+    getuserChannelProfile,
+    getWatchHistory,
+} from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { upload } from "../middlewares/multer.middleware.js"; 
+import { upload } from "../middlewares/multer.middleware.js";
 import { refreshAccessToken } from "../controllers/user.controller.js";
-const router=Router();
+import { uploadOnCloudinary } from "../models/cloudinary.js";
+const router = Router();
 
-router.route("/register").post
-(
+router.route("/register").post(
     upload.fields([
         {
-            name:"avatar",
-            maxCount:1
+            name: "avatar",
+            maxCount: 1,
         },
         {
-            name:"coverImage",
-            maxCount:1
-        }
+            name: "coverImage",
+            maxCount: 1,
+        },
     ]),
 
-    registerUser)
-router.route("/login").post(loginUser)
-router.route("/logout").post(verifyJWT,logoutUser)
-router.route("/refresh-Token").post(refreshAccessToken)
+    registerUser
+);
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-Token").post(refreshAccessToken);
+router.route("/changed-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+    .route("/avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+    .route("/cover-image")
+    .patch(verifyJWT, upload.single("/coverImage"), updateCoverImage);
 
+router.route("/c/:username").get(verifyJWT, getuserChannelProfile);
+router.route("/watchHistory").get(verifyJWT, getWatchHistory);
 export default router;
-
